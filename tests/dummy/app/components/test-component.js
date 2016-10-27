cc9a756f88387710a72fe329222fbe6c734caf22
Chantal Broeren <chantal.broeren@fabriquartz.com>
2016-10-28 00:39:27 +0200

    demo

--- tests/dummy/app/components/test-component.js
+++ tests/dummy/app/components/test-component.js
@@ -1,30 +1,59 @@
 import Component from 'ember-component';
+import computed from 'ember-computed';
 import contextMenuMixin from 'ember-context-menu';
+
+function increment(objects, amount = 1) {
+  objects.forEach((object) => {
+    object.incrementProperty('count', amount);
+  });
+}
+
+function setTo(objects, value = 0) {
+  objects.forEach((object) => {
+    object.set('count', value);
+  });
+}
 
 export default Component.extend(contextMenuMixin, {
   classNames: ['test-component'],
+  classNameBindings: ['object.selected:test-component--selected'],
 
-  contextSelection: [1, 2],
+  contextSelection: [],
 
-  contextItems: [
-    { label: 'Disabled item' },
-    {
-      label: 'Multi actions',
-      subActions: [
-        { label: 'Sub action', action() { } },
+  contextItems: computed('object.selected', function() {
+    if (this.get('object.selected')) {
+      return [
+        { label: 'No action' },
+        { label: 'Singe object action',
+          disabled(objects) {
+            return objects.length > 1;
+          },
+          action() { } },
         {
-          label: 'Multiple sub',
+          label: 'Multi actions',
           subActions: [
-            { label: 'Sub A', action() { } },
-            { label: 'Sub B', action() { } }
+            {
+              label: 'Set to 5',
+              action(objects) { setTo(objects, 5); }
+            },
+            {
+              label: 'Add',
+              subActions: [
+                { label: 'Add 1', action(objects) { increment(objects, 1); } },
+                { label: 'Add 3', action(objects) { increment(objects, 3); } },
+                { label: 'Add 10', action(objects) { increment(objects, 10); } }
+              ]
+            }
           ]
+        },
+        {
+          label: 'Reset to 0',
+          icon: 'undo',
+          action(objects) { setTo(objects, 0); }
         }
-      ]
-    },
-    {
-      label: 'Do something',
-      icon: 'search',
-      action() { }
+      ];
     }
-  ]
+
+    return [];
+  })
 });
---
+++ tests/dummy/app/routes/application.js
@@ -0,0 +1,15 @@
+import Route from 'ember-route';
+import { A } from 'ember-array/utils';
+import EmberObject from 'ember-object';
+
+export default Route.extend({
+  model() {
+    return A([
+      EmberObject.create({ count: 0 }),
+      EmberObject.create({ count: 0 }),
+      EmberObject.create({ count: 0 }),
+      EmberObject.create({ count: 0 }),
+      EmberObject.create({ count: 0 })
+    ]);
+  }
+});
