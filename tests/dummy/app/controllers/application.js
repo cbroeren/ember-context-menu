8c9f7037f4421bb14a12e4a97db425a7717c4b75
Chantal Broeren <cbroeren@users.noreply.github.com>
2016-10-28 19:21:50 +0200

    Improve demo (#12)

---
+++ tests/dummy/app/controllers/application.js
@@ -0,0 +1,14 @@
+import Controller from 'ember-controller';
+import computed from 'ember-computed';
+
+export default Controller.extend({
+  selection: computed('model.@each.selected', function() {
+    return this.get('model').filterBy('selected');
+  }),
+
+  actions: {
+    toggleSelectObject(object) {
+      object.toggleProperty('selected');
+    }
+  }
+});
