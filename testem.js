ee07ef03b4f6b8c9c4f488db0ee8d615d765a733
Chantal Broeren <chantal.broeren@fabriquartz.com>
2017-07-18 12:30:11 +0200

    update ember to 2.14

--- testem.js
+++ testem.js
@@ -1,7 +1,7 @@
 /* eslint-env node */
 module.exports = {
-  "test_page": "tests/index.html?hidepassed",
-  "disable_watching": true,
+  test_page: "tests/index.html?hidepassed",
+  disable_watching: true,
   "launch_in_ci": [
     "Chrome"
   ],
